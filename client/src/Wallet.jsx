55e0e64b2a8a04321ddc8cd2eb2ac3fe77484fe4
Syed Ghufran Hassan <syedghufranhassan@gmail.com>
2024-02-19 11:19:37 +0500

    push

--- client/src/Wallet.jsx
+++ client/src/Wallet.jsx
@@ -1,8 +1,13 @@
 import server from "./server";
+import * as secp from 'ethereum-cryptography/secp256k1';
+import { toHex } from 'ethereum-cryptography/utils';
 
-function Wallet({ address, setAddress, balance, setBalance }) {
+function Wallet({ address, setAddress, balance, setBalance, privateKey, setPrivateKey }) {
   async function onChange(evt) {
-    const address = evt.target.value;
+    const privateKey = evt.target.value;
+    setPrivateKey(privateKey);
+   const address = toHex(secp.getPublicKey(privateKey));
+  // const address = keccak256(privateKey.slice(1)).slice(-20);
     setAddress(address);
     if (address) {
       const {
@@ -19,9 +24,11 @@ function Wallet({ address, setAddress, balance, setBalance }) {
       <h1>Your Wallet</h1>
 
       <label>
-        Wallet Address
-        <input placeholder="Type an address, for example: 0x1" value={address} onChange={onChange}></input>
+        Private Key
+        <input placeholder="Type the private key" value={privateKey} onChange={onChange}></input>
       </label>
+
+      <div>Address: {address.slice(0,10)}...</div>
 
       <div className="balance">Balance: {balance}</div>
     </div>
--- server/index.js
+++ server/index.js
@@ -7,9 +7,9 @@ app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  "03ec1008e2a74845a65d8424c7f0ecdce443cd410fda2aac337820d0ada7c184fe": 100,
+  "02e85017ffaa806318915d1b7b33eeba197954dcb96ea726f995dad987eac8228e": 50,
+  "037016cec2ed338a13c4df194f5ee879204a60110fefc65e1c09d007a2fa5eb352": 75,
 };
 
 app.get("/balance/:address", (req, res) => {
